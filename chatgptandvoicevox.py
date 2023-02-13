d04498f8ace2c6576a46fb393ca5679ccbd22474
お墓さま <102584033+I-am-a-cool-guy@users.noreply.github.com>
2023-02-13 14:54:40 +0900

    New premium TENGA

--- chatgptandvoicevox.py
+++ chatgptandvoicevox.py
@@ -6,7 +6,7 @@ import openai
 import json
 import requests
 import wave
-import pytz
+
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=["!",""],intents=intents)
@@ -57,41 +57,22 @@ def generate_wav(text, speaker=1, filepath='./audio.mp3'):
 @bot.event
 async def on_message(message):
     if message.guild:
-        Gld = message.guild
-        Chel = message.channel
-        author = message.author
-        #Server and channel id to record monitoring
-        guilds = bot.get_guild()#ID!!!
-        chunked = guilds.get_channel()#CNID!!!!
-
+        
         if message.author.bot:
             return
         else:
-            #全サーバーの全メッセージを取得
-            if message.guild:
-                embed_private = discord.Embed(title="Message-log",color=0xff4242)
-                embed_private.add_field(name="server",value=f"{Gld.name} | {Gld.id} | ({Gld.member_count})",inline=False)
-                embed_private.add_field(name="channel",value=f"{Chel.name} | {Chel.id}",inline=False)
-                embed_private.add_field(name="user",value=f"{author} | <@{author.id}>",inline=False)
-                embed_private.add_field(name="message",value=f"{message.content}",inline=False)
-                #This is a handsome command that monitors messages on all servers the bot participates in. 
-                await chunked.send(embed=embed_private)
-        
-        if not message.guild:
-
             print(message.content)
             async with message.channel.typing():
-                query = message.content
-                response = GPT(query)
+                response = GPT(message.content)
                 await message.author.send(response)
                 generate_wav(response)
-                await message.channel.send(file=discord.File("./audio.mp3"))
-                #I want to talk about (response) in voice chat, but I don't know how to play it in mp3 or wav. 
-                #HELP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                message.guild.voice_client.play(discord.FFmpegPCMAudio("audio.mp3"))
                     
 
 bot.run("") #TOKEN
 API_KEY = "" #OpenAI's APIKEY
 
 #python v 3.7.4
-#discord.py Maybe, the latest, maybe
+#discord.py 2?
+#windows10 pro 
+#penis 14.5cm
